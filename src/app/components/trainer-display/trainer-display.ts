c83b5921d602fdbd286b7c8e04ae7348f6571a59
quijano_j <quijano_j@f213.ub.lan>
2026-09-24 09:10:47 +0800

    feat: Added Display in Trainer Display

--- src/app/components/trainer-display/trainer-display.ts
+++ src/app/components/trainer-display/trainer-display.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TrainerService } from '../trainer-service/trainer-service';
 
 @Component({
-  imports: [],
-  selector: 'app-trainer-display',
-  styleUrl: './trainer-display.css',
-  templateUrl: './trainer-display.html',
+selector: 'app-trainer-display',
+standalone: true,
+template: `
+<div class="container">
+  <h1>Hoenn Region</h1>
+
+  @for (trainer of trainerService.trainers(); track trainer.name) {
+    <div class="trainer-card">
+      <h2>Gym leader: {{ trainer.name }}</h2>
+      <p>
+        <strong>Town:</strong> {{ trainer.town }} | 
+        <strong>Type:</strong> {{ trainer.specialty }} | 
+        <strong>Badge:</strong> {{ trainer.badge }}
+      </p>
+      
+      <ul>
+        @for (pokemon of trainer.team; track $index) {
+          <li>
+            <strong>{{ pokemon }}</strong>
+          </li>
+        }
+      </ul>
+    </div>
+  }
+</div>
+`
 })
-export class TrainerDisplay {}
+export class TrainerDisplay {
+trainerService = inject(TrainerService);
+}
